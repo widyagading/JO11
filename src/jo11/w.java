6bdac894d6bf178862661144283dbfc8a82a22fa
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-14 18:39:40 +0700

    Initial Commit

---
+++ src/jo11/w.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jo11;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author - INDIEGLO -
+ */
+public class w {
+     private static void tampilJudul(String identitas) {
+        System.out.println("identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+    public static void main(String[] args){
+    String identitas = "Widya Gading Sejati / XRPL2 / 38";
+    tampilJudul(identitas);
+    }
+}
