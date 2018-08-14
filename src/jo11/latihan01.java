a3f84a096b8632709a1dc52550f32ab3287010c4
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-14 18:23:11 +0700

    Coba method compareTo & compareToIgnoreCase

--- src/jo11/latihan01.java
+++ src/jo11/latihan01.java
@@ -29,6 +29,15 @@ public class latihan01 {
      System.out.println("isi x sama dengan r (Case Sensitve) : " + x.equals(r));
      System.out.println("isi x sama dengan r (Not Case Sensitive) : " + x.equalsIgnoreCase(r));
      
+     System.out.println("perbandingan isi x dengan y : " + x.compareTo(y));
+     System.out.println("perbandingan isi x dengan z (Case sensitive) : " + x.compareTo(z));
+     System.out.println("perbandingan isi x dengan r (Case Sensitive) : " + x.compareTo(r));
+     System.out.println("perbandingan isi x dengan r (Not Case Sensitive) : " + x.compareToIgnoreCase(r));
+     
+     String s = "operasI";
+     System.out.println("isi variabel s : "+ s);
+     System.out.println("perbandingan isi r dengan s (Case Sensitive) : " + r.compareTo(s));
+     
       
       }
     
