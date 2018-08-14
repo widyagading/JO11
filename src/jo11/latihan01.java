94076d61261cbe57cde74ade5e56dd229e6cf3e0
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-14 18:21:31 +0700

    Coba method equals & equalsIgnoreCase

--- src/jo11/latihan01.java
+++ src/jo11/latihan01.java
@@ -18,6 +18,17 @@ public class latihan01 {
       System.out.println("isi variabel y : "+ y);
       System.out.println("y adalah kosong : "+ y.isEmpty());
       
+      System.out.println("isi x sama dengan y :" + x.equals(y));
+     
+     String z = "operasi";
+     System.out.println("isi variabel z : " + z);
+     System.out.println("isi x sama dengan z (Case Sensitive): " + x.equals(z));
+     
+     String r = "operasi";
+     System.out.println("isi variabel r : "+ r);
+     System.out.println("isi x sama dengan r (Case Sensitve) : " + x.equals(r));
+     System.out.println("isi x sama dengan r (Not Case Sensitive) : " + x.equalsIgnoreCase(r));
+     
       
       }
     
