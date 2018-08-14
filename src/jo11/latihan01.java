2afad3ed248c2fb5a460ca6560647613a93583e7
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-14 18:27:21 +0700

    Coba method format

--- src/jo11/latihan01.java
+++ src/jo11/latihan01.java
@@ -66,7 +66,10 @@ public class latihan01 {
         for(int i=0; i<arrTr.length; i++)
          System.out.println(arrTr[i]);
       
-      
+       double d = 1545454000;
+        System.out.println(String.format("Uang saya %,.4f", d));
+        
+        System.out.println(String.format(Locale.forLanguageTag("in-ID"), "uang saya Rp %,.2f", d));
       
       }
     
