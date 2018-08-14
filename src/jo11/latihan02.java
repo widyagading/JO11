6e1c29fe5956181eaf5fd8197798787e663d5710
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-14 18:41:58 +0700

    Initial Commit

---
+++ src/jo11/latihan02.java
@@ -0,0 +1,14 @@
+
+package jo11;
+import java.util.Scanner;
+public class latihan02 {
+    private static void tampilJudul(String identitas) {
+        System.out.println("identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+    public static void main(String[] args){
+    String identitas = "Widya Gading Sejati / XRPL2 / 38";
+    tampilJudul(identitas);
+    }
+}
