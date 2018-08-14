f3d0405c525ce36eab359e0788f97bd0c0a347b7
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-14 18:16:48 +0700

    Initial Commit

---
+++ src/jo11/latihan01.java
@@ -0,0 +1,13 @@
+
+package jo11;
+
+import java.util.Locale;
+
+public class latihan01 {
+    public static void main(String[] args) {
+      String identitas = "Widya Gading Sejati / XRPL2 / 38";
+      System.out.println("Identitas : "+ identitas);
+      
+      }
+    
+}
