baaf161f246de617fb2528c4985c08270fda8d5c
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-14 18:26:19 +0700

    Coba method substring,replace, & split

--- src/jo11/latihan01.java
+++ src/jo11/latihan01.java
@@ -49,6 +49,24 @@ public class latihan01 {
         
          System.out.println("gabungkan isi variabel  x dan r : "+ x.concat(r));
      
+         System.out.println("karaktr mulai posisi 3 di vaiabel x : " + x.substring(3));
+      System.out.println("karekter dari posisi 3-5 di variabel x : " + x.substring(3,6));
+      
+      String tclean = t.trim();
+      System.out.println("isi variabel tclean : "+ t.trim());
+      System.out.println("rubah 'i' jadi 'E' di variabel tclean : " + tclean.replace("i", "E"));
+      
+      String[] arrT = tclean.split(" ");
+       System.out.println("pecah kata di variabel tclean : ");
+       for(int i=0; i<arrT.length; i++)
+          System.out.println(arrT[i]);
+       String[] arrTr = tclean.split("r");
+        System.out.println("pecah berdasarkan huruf 'r' di variabel tclean : ");
+        
+        for(int i=0; i<arrTr.length; i++)
+         System.out.println(arrTr[i]);
+      
+      
       
       }
     
