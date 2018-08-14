fed79789417ca52a76d2731c7509ce44e8e48bad
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-14 18:19:54 +0700

    Coba method length & isEmpty

--- src/jo11/latihan01.java
+++ src/jo11/latihan01.java
@@ -8,6 +8,17 @@ public class latihan01 {
       String identitas = "Widya Gading Sejati / XRPL2 / 38";
       System.out.println("Identitas : "+ identitas);
       
+      String x = "operasi";
+      System.out.println("isi variabel x : " + x);
+      System.out.println("\"" + x + "\" panjang karakter = "+ x.length());
+      
+      System.out.println("x adalah kosong : " + x.isEmpty());
+      
+      String y = "";
+      System.out.println("isi variabel y : "+ y);
+      System.out.println("y adalah kosong : "+ y.isEmpty());
+      
+      
       }
     
 }
