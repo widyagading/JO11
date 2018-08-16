47cd7487b94cb22e349ab76b0565dfcc4f8e6fc5
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-16 22:01:18 +0700

    initial commit

---
+++ src/jo11/z.java
@@ -0,0 +1,17 @@
+
+package jo11;
+
+import java.util.Scanner;
+
+public class z {
+    private static void tampilJudul(String identitas) {
+      System.out.println("Identitas : " + identitas);
+      
+      System.out.println("\nConvert kalimat alay angka (Vokal ke angka)\n");
+}
+     public static void main(String[] args){
+        String identitas = "Widya Gading Sejati / XRPL2 / 38";
+        tampilJudul(identitas);
+        }
+}
+
