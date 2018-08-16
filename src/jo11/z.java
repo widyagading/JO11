03f6e743b0bf3d65631ddf504afde842195172c6
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-16 23:19:22 +0700

    coba method length & isEmpty

--- src/jo11/z.java
+++ src/jo11/z.java
@@ -4,9 +4,16 @@ package jo11;
 import java.util.Scanner;
 
 public class z {
-    public static void main(String[] args) {
-      String identitas = "Widya Gading Sejati / XRPL2 / 38";
-      System.out.println("Identitas : "+ identitas);
-  }
+    public static void main(String[] args){
+   String x = "operasi";
+      System.out.println("isi variabel x : " + x);
+      System.out.println("\"" + x + "\" panjang karakter = "+ x.length());
+      
+      System.out.println("x adalah kosong : " + x.isEmpty());
+      
+      String y = "";
+      System.out.println("isi variabel y : "+ y);
+      System.out.println("y adalah kosong : "+ y.isEmpty());
+}
 }
 
