c3fb9cc822223bfac0b3384880004b0a8d07144f
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-16 22:13:32 +0700

    Tambah konversi

--- src/jo11/z.java
+++ src/jo11/z.java
@@ -4,17 +4,21 @@ package jo11;
 import java.util.Scanner;
 
 public class z {
-private static String tampilInput(){
-        Scanner scanner = new Scanner(System.in);
+    private static String vocal2Angka(String kalimat){
+            
+        char [][] arConvert =
+                
+        {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
         
-        System.out.println("Masukkan Kalimat: ");
-        String kalimat = scanner.nextLine();
-        System.out.println("Kalimat Asli : " + kalimat);
+        kalimat = kalimat.toLowerCase();
+        for(int i=0;i<arConvert.length;i++)
+            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
         
         return kalimat;
-    }    
-public static void main(String[] args){
-    String kalimat = tampilInput();
-}
+    }
+    
+     public static void main(String[] args){
+       String convert = vocal2Angka(kalimat());
+    }
 }
 
