9c919984c67b1c118fda0af5557f85a88b3de45a
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-16 22:18:23 +0700

    Tambah tampil hasil

--- src/jo11/z.java
+++ src/jo11/z.java
@@ -4,17 +4,13 @@ package jo11;
 import java.util.Scanner;
 
 public class z {
-   private static void tampilPerKata(String kalimat, String convert){
-        String[]arrKal = kalimat.split(" ");
-        String[]arrCon = convert.split(" ");
-        
-        for(int i=0;i<arrKal.length;i++)
-            System.out.println(arrKal[i]+" => "+arrCon[i]);
-         tampilPerKata(kalimat,convert);
-         
-    }    
-   public static void main(String[] args){
-       tampilPerkata(kalimat, convert);
-   }
+    
+ private static void tampilHasil(String convert){
+           System.out.println("Kalimat Alay Angka : " + convert);
+           tampilHasil(convert);
+}
+ public static void main(String[] args){
+     tampilHasil(convert);
+ }
 }
 
