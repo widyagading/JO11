fdcf014d8350970db7eb624913fe8a408e1cfaca
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-16 23:30:43 +0700

    coba method format

--- src/jo11/z.java
+++ src/jo11/z.java
@@ -1,6 +1,7 @@
 
 package jo11;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class z {
@@ -11,22 +12,10 @@ public class z {
  String r = "operasi";
  String s = "operasI";
  String t = "    operasi string   ";
-  System.out.println("karaktr mulai posisi 3 di vaiabel x : " + x.substring(3));
-      System.out.println("karekter dari posisi 3-5 di variabel x : " + x.substring(3,6));
-      
-      String tclean = t.trim();
-      System.out.println("isi variabel tclean : "+ t.trim());
-      System.out.println("rubah 'i' jadi 'E' di variabel tclean : " + tclean.replace("i", "E"));
-      
-      String[] arrT = tclean.split(" ");
-       System.out.println("pecah kata di variabel tclean : ");
-       for(int i=0; i<arrT.length; i++)
-          System.out.println(arrT[i]);
-       String[] arrTr = tclean.split("r");
-        System.out.println("pecah berdasarkan huruf 'r' di variabel tclean : ");
+double d = 1545454000;
+        System.out.println(String.format("Uang saya %,.4f", d));
         
-        for(int i=0; i<arrTr.length; i++)
-         System.out.println(arrTr[i]);
+        System.out.println(String.format(Locale.forLanguageTag("in-ID"), "uang saya Rp %,.2f", d));
      
      
 }
