53abef13707ba868c06744784b89ff450a0ac88d
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-16 20:14:25 +0700

    initial commit

---
+++ src/jo11/y.java
@@ -0,0 +1,10 @@
+
+package jo11;
+
+public class y {
+    public static void main(String[] args) {
+      String identitas = "Widya Gading Sejati / XRPL2 / 38";
+      System.out.println("Identitas : "+ identitas);
+}
+    
+    
