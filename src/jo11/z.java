95011622ce7ceae18d45acacd34ae53d8ea52369
- INDIEGLO - <- INDIEGLO -@widya>
2018-08-16 23:27:28 +0700

    coba method contains & toUpperCase,toLowerCase, trim & concat

--- src/jo11/z.java
+++ src/jo11/z.java
@@ -9,14 +9,18 @@ public class z {
  String y = "";
  String z = "operasi";
  String r = "operasi";
-  System.out.println("perbandingan isi x dengan y : " + x.compareTo(y));
-     System.out.println("perbandingan isi x dengan z (Case sensitive) : " + x.compareTo(z));
-     System.out.println("perbandingan isi x dengan r (Case Sensitive) : " + x.compareTo(r));
-     System.out.println("perbandingan isi x dengan r (Not Case Sensitive) : " + x.compareToIgnoreCase(r));
+ String s = "operasI";
+ System.out.println("pada x terdapat huruf 'a' : " + x.contains("a"));
      
-     String s = "operasI";
-     System.out.println("isi variabel s : "+ s);
-     System.out.println("perbandingan isi r dengan s (Case Sensitive) : " + r.compareTo(s));
+      System.out.println("isi variabel x besar semua : "+ x.toUpperCase());
+      System.out.println("isi variabel x kecil semua : "+ x.toLowerCase());
+      
+      String t = "    operasi string   ";
+       System.out.println("isi variabel t : \""+ t +"");
+        System.out.println("isi variabel t tanpa spasi : \""+ t.trim() +"\"");
+        
+         System.out.println("gabungkan isi variabel  x dan r : "+ x.concat(r));
+     
      
 }
 }
